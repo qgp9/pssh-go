cb6abdef5d8ac389df206f88deff251d2eace842
Beomsu Chang <pauljang9@gmail.com>
2022-08-16 16:01:12 +0300

    parser/entry-options.go: fix bug in ReplaceAllString

--- parser/entry-options.go
+++ parser/entry-options.go
@@ -1,20 +1,23 @@
 package parser
 
-import "regexp"
+import (
+	"regexp"
+)
 
 type parserOption struct {
 	parserHelper[*entryOption]
 }
 
-var reOptionPrefix = regexp.MustCompile(`^(\s*\|\s*)*-\s*`)
+var reOptionPrefix = regexp.MustCompile(`^([\s\|]*)-\s*(.*?)([\s\|]*)$`)
 
 func (e *parserOption) Selector(line string) bool {
 	return reOptionPrefix.MatchString(line)
 }
 
 func (e *parserOption) Parse(c *PConfig) int {
+	// TODO: comment
 	var entry = entryOption{}
-	entry.value = reOptionPrefix.ReplaceAllString(c.currentLine(), "")
+	entry.value = reOptionPrefix.ReplaceAllString(c.currentLine(), "$2")
 	entry.value = c.applyVariable(entry.value)
 	c.addEntry(&entry)
 	return 1
